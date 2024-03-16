810b38b543cfc306f555953cc3ae5d4edad3d509
syau0005 <syau0005@student.monash.edu>
2024-03-16 10:06:31 +0800

    chore

--- shopping-list/src/App.js
+++ shopping-list/src/App.js
@@ -1,90 +1,19 @@
 import './App.css';
-import React, { useState } from 'react';
-import SearchBox from './components/SearchBox';
-import CardList from './components/CardList';
-import data from './data/data.json';
-import { Select } from 'antd';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './containers/Home';
+import Contact from './containers/Contact';
+import Blogs from './containers/Blogs';
 
 function App() {
-  const [search, setSearch] = useState(''); // const [stateVariable, setStateFunction] = useState(initialValue);
-  const [filteredValues, setFilteredValues] = useState([]);
-
-  const onSearchChange = (e) => {
-    console.log(e.target.value);
-    setSearch(e.target.value);
-  };
-
-  // data = [1, 2, 3, 4]
-  // newData = [1, 4, 9, 16]
-  // 1 *1
-  // 2 *2
-  // 3 *3
-  // 4 *4
-
-  // data.map((item) => {
-  //   return item * item;
-  // });
-
-  /**
-   * [{}, {}, {}, {}, ...] JSON
-   * [{}, {}, {}, {}, ...] JS
-   *
-   *
-   * item = {}
-   *
-   */
-
-  const categories = [];
-
-  const cards = data.map((item) => {
-    if (!categories.includes(item.category)) {
-      categories.push(item.category);
-    }
-
-    return {
-      name: item.name,
-      price: item.price,
-      category: item.category,
-    };
-  });
-
-  const filteredCards = cards
-    .filter((card) => {
-      return card.name.toLowerCase().includes(search.toLowerCase());
-    })
-    .filter((card) => {
-      return filteredValues.length === 0
-        ? true
-        : filteredValues.includes(card.category);
-    });
-
-  const handleChange = (selectedValues) => {
-    setFilteredValues(selectedValues);
-  };
-
-  const options = categories.map((category) => {
-    return {
-      label: category,
-      value: category,
-    };
-  });
-
   return (
-    <div className='App'>
-      <SearchBox search={search} onSearchChange={onSearchChange} />
-      <Select
-        mode='multiple'
-        allowClear
-        style={{
-          width: '50%',
-        }}
-        placeholder='Please select'
-        defaultValue={[]}
-        onChange={handleChange}
-        options={options}
-      />
-      <CardList cards={filteredCards} />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/blogs' element={<Blogs />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
